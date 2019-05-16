a083c986660c6898db924c17089df23ed20b8a74
Lobisgato <marciano_store@outlook.com/>
2019-05-16 14:06:36 -0300

    ultimas alteracoes

--- Project/UFSHotel/src/Sistema/our_Reserva.java
+++ Project/UFSHotel/src/Sistema/our_Reserva.java
@@ -25,37 +25,37 @@ public class our_Reserva extends TipoReserva {
         super(dataIn, dataOut, idReserva, cli, recep, quarto, pag);
 
     }
-    
+
     public double getTotalConsumido() {
-    	double total = 0;
-    	Map<String, Double> Consumido;
+        double total = 0;
+        Map<String, Double> Consumido;
         Consumido = getConsumido();
         for (Map.Entry<String, Double> entry : Consumido.entrySet()) {
             Double v = entry.getValue();
             total += v;
         }
-        
+
         return total;
     }
-    
+
     public float calcularDesconto(float percentDisc) {
-    	float preco;
+        float preco;
         TipoPagamento pag = getPag();
-    	preco = pag.getValor();
-    	
-    	return preco*(percentDisc/100);
+        preco = pag.getValor();
+
+        return preco * (percentDisc / 100);
     }
-    
+
     public boolean addDesconto(int percentDisc) {
-    	float Desconto, valorNovo;
+        float Desconto, valorNovo;
         TipoPagamento pag = getPag();
-    	
-    	Desconto = calcularDesconto(percentDisc);
-    	valorNovo = pag.getValor() - Desconto;
-    	
-    	pag.setValor(valorNovo);
-    	
-    	return true;
+
+        Desconto = calcularDesconto(percentDisc);
+        valorNovo = pag.getValor() - Desconto;
+
+        pag.setValor(valorNovo);
+
+        return true;
     }
 
 }
